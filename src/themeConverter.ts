b1bec510925a84e1e3e92f3a38f8241078d00b4c
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-08-12 21:11:23 +0000

    feat: add cloning of theme-converter-for-vs

---
+++ src/themeConverter.ts
@@ -0,0 +1,5 @@
+import { exec } from "@actions/exec";
+
+export async function cloneRepository(): Promise<void> {
+  await exec('git clone', ['https://github.com/microsoft/theme-converter-for-vs']);
+}
