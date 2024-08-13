9d9690c7d396d15cbc65e4ef4f19523d1c03c559
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-08-13 20:18:51 +0000

    feat: add error handling, logging and default work dir

--- src/themeConverter.ts
+++ src/themeConverter.ts
@@ -1,8 +1,19 @@
 import { exec } from '@actions/exec'
+import { setFailed, info } from '@actions/core'
 
-export async function cloneRepository(): Promise<void> {
+const WORK_DIRECTORY = 'work'
+
+export async function cloneRepository(
+  cloneDir = WORK_DIRECTORY
+): Promise<void> {
+  info('Cloning ThemeConverter repo...')
+
   await exec('git clone', [
     'https://github.com/microsoft/theme-converter-for-vs',
-    'work'
-  ])
+    cloneDir
+  ]).catch((error: Error) => {
+    setFailed(`Action failed with error: "${error.message}"`)
+  })
+}
+
 }
