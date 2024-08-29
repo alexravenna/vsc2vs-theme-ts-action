889e628a9dd3392f559d3ad60434baa1b9062b8e
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-08-29 20:10:04 +0000

    add method docs

--- src/themeConverter.ts
+++ src/themeConverter.ts
@@ -1,6 +1,11 @@
 import { exec } from '@actions/exec'
 import { setFailed, info } from '@actions/core'
 
+/**
+ * Clones the ThemeConverter project.
+ * 
+ * @param cloneDir the directory to clone the ThemeConverter project to
+ */
 export async function cloneRepository(cloneDir: string): Promise<void> {
   info('Cloning ThemeConverter repo...')
 
@@ -12,6 +17,11 @@ export async function cloneRepository(cloneDir: string): Promise<void> {
   })
 }
 
+/**
+ * Builds the ThemeConverter project.
+ * 
+ * @param buildDir the directory to build the ThemeConverter project in
+ */
 export async function buildProject(buildDir: string): Promise<void> {
   info('Building ThemeConverter project...')
   
